66e587d73c90a8eefa4dca1b9c3b1efc65a53d63
Gregory Roberts <roberts.gregory@heb.com>
2021-10-12 14:21:49 -0500

    Create db connection for tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,1 +1,30 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
+const dbDriver = "postgres"
+const dbSource = "postgresql://root:secret@localhost:5432/cart-buddy?sslmode=disable"
+
+// testQueries Establish connection to the Queries object
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+
+	// Establish a simple database connection
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	// Check for errors in opening DB connection
+	if err != nil {
+		log.Fatal("cannot connect to DB: ", err)
+	}
+
+	// Define the object and assign the DB connection
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
