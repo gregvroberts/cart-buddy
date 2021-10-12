1a0de5f1d68afb58ee9e7b67d644d686debb4ec4
Gregory Roberts <roberts.gregory@heb.com>
2021-10-12 14:37:40 -0500

    Test Finished: TestCreateUser

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -1,10 +1,45 @@
 package db
 
 import (
-	"log"
+	"context"
+	"database/sql"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestCreateUser(t *testing.T) {
-	log.Fatal("Not implemented")
+	arg := CreateUserParams{
+		UserFName:   "CreateUserFirst",
+		UserLName:   "CreateUserLast",
+		UserEmail:   "createUserEmail@test.com",
+		UserCity:    "CreateUserCity",
+		UserState:   "CreateUserState",
+		UserPostal:  "CreateUserPostal",
+		UserCountry: "CreateUserCountry",
+		UserAddr1:   "CreateUserAddress1",
+	}
+
+	argEmailVerified := sql.NullBool{Bool: false, Valid: true}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+
+	// Imported from testify. If requirements are not met, the test will automatically fail.
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.UserFName, user.UserFName)
+	require.Equal(t, arg.UserLName, user.UserLName)
+	require.Equal(t, arg.UserEmail, user.UserEmail)
+	require.Equal(t, arg.UserCity, user.UserCity)
+	require.Equal(t, arg.UserState, user.UserState)
+	require.Equal(t, arg.UserPostal, user.UserPostal)
+	require.Equal(t, arg.UserCountry, user.UserCountry)
+	require.Equal(t, arg.UserAddr1, user.UserAddr1)
+
+	require.NotZero(t, user.UserID)
+	require.NotZero(t, user.CreatedAt)
+	require.NotZero(t, user.UpdatedAt)
+
+	require.Equal(t, user.UserEmailVerified, argEmailVerified)
+
 }
